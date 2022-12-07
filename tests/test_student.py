cc4126a1f1e68a21fad30735c9074bcaf4976526
Catherine Bandarchuk <kate@sealucky.com>
2022-12-07 10:13:30 -0800

    fix the code coverage

--- tests/test_student.py
+++ tests/test_student.py
@@ -11,6 +11,16 @@ def test_init():
     assert ada.level == level
     assert ada.courses == courses
 
+def test_init_empty_courses_input():
+    name = "Ada Lovelace"
+    level = "sophomore"
+    
+    ada = Student(name, level)
+
+    assert ada.name == name
+    assert ada.level == level
+    assert ada.courses == []
+
 def test_add_class():
     new_class = 'Intro to Feminism'
     charles = Student("Charles Babbage", "senior", ["mechanical engineering"])
@@ -41,4 +51,17 @@ def test_get_student_with_more_classes():
         ["mathematics", "foundations of computing"]
     )
 
-    # TODO: write assertions
+    result = get_student_with_more_classes(charles, ada)
+    assert result == ada
+
+
+def test_get_first_student_with_more_classes():
+    charles = Student("Charles Babbage", "senior", ["mechanical engineering", "LA", "Math"])
+    ada = Student(
+        "Ada Lovelace",
+        "sophomore",
+        ["mathematics", "foundations of computing"]
+    )
+
+    result = get_student_with_more_classes(charles, ada)
+    assert result == charles
